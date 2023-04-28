e24708b2aa46f56ed19f89a7e3a5e897bd710f4b
FilBelan <filbelan@gmail.com>
2023-04-28 15:44:37 +0300

    addFilesFromT3

---
+++ src/components/Filter/Filter.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const FilterTitle = styled.label`
+  margin-bottom: 10px;
+  color: green;
+  font-size: 24px;
+`;
+
+export const FilterInput = styled.input`
+  width: 400px;
+  height: 20px;
+`;
